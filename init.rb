07c22a146482704dcc68c5b54762ab569ad086f5
tobixx0 <Tobi.Stenzel@gmx.de>
2013-03-28 03:14:12 +0100

    generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_pirate_helpdesk do
+  name 'Redmine Pirate Helpdesk plugin'
+  author 'Author name'
+  description 'Plugin for a simple pirate helpdesk system'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
